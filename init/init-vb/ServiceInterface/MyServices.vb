e68cd7afd740201fb80b74e0a955c1b95f6747b4
Demis Bellot <demis.bellot@gmail.com>
2020-08-22 16:04:39 +0800

    Add init-vb mix gist

---
+++ init/init-vb/ServiceInterface/MyServices.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports ServiceStack
+Imports MyApp.ServiceModel
+
+Namespace Global.MyApp.ServiceInterface
+
+    Public Class MyServices 
+        Inherits Service
+
+        public Function Any(request AS Hello) AS Object
+            Return new HelloResponse With { .Result = $"Hello, {request.Name}!" }
+        End Function
+
+    End Class
+
+End Namespace
---
+++ init/init-vb/ServiceModel/Hello.vb
@@ -0,0 +1,16 @@
+﻿Imports ServiceStack
+
+Namespace Global.MyApp.ServiceModel
+
+    <Route("/hello")>
+    <Route("/hello/{Name}")>
+    Public Partial Class Hello
+        Implements IReturn(Of HelloResponse)
+        Public Overridable Property Name As String
+    End Class
+
+    Public Partial Class HelloResponse
+        Public Overridable Property Result As String
+    End Class
+
+End Namespace
