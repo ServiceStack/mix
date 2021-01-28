e9ee2ba723d7582260f69900ba0c764c1fd35d0c
Demis Bellot <demis.bellot@gmail.com>
2021-01-29 02:28:02 +0800

    Add Console JS

--- init/console-js/index.js
+++ init/console-js/index.js
@@ -1,3 +1,29 @@
 const { Inspect } = require('gistcafe');
+const fetch = require('node-fetch');
 
-console.log('Hello, World!');
+(async () => {
+
+    let orgName = "nodejs";
+
+    let orgRepos = (await (await fetch(`https://api.github.com/orgs/${orgName}/repos`)).json())
+        .map(x => ({
+            name: x.name,
+            description: x.description,
+            lang: x.language,
+            watchers: x.watchers_count,
+            forks: x.forks
+        }));
+
+    orgRepos.sort((a, b) => b.watchers - a.watchers);
+
+    console.log(`Top 3 ${orgName} Github Repos:`);
+    Inspect.printDump(orgRepos.slice(0, 3));
+
+    console.log(`\nTop 10 ${orgName} Github Repos:`);
+    Inspect.printDumpTable(orgRepos.map(x => ({
+        name: x.name, lang: x.lang, watchers: x.watchers, forks: x.forks
+    })).slice(0, 10));
+
+    Inspect.vars({ orgRepos });
+
+})();
