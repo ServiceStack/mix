99b1efb05aa5bfba2d2a78a33ea2fd0fcf91614f
Demis Bellot <demis.bellot@gmail.com>
2021-12-01 14:19:44 +0800

    update mix gists

--- init/console-cs/Program.cs
+++ init/console-cs/Program.cs
@@ -20,11 +20,11 @@ Inspect.vars(new { orgRepos });
 
 public class GithubRepo
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string Url { get; set; }
-    public string Homepage { get; set; }
-    public string Language { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Url { get; set; }
+    public string? Homepage { get; set; }
+    public string? Language { get; set; }
     public int Watchers { get; set; }
     public int Forks { get; set; }
 }
