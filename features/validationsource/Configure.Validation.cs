eab50ad75d011094f8ac105a03f941339643062f
Demis Bellot <demis.bellot@gmail.com>
2022-07-04 20:11:14 +0800

    Update Configure.Validation.cs

--- features/validationsource/Configure.Validation.cs
+++ features/validationsource/Configure.Validation.cs
@@ -3,16 +3,15 @@ using ServiceStack.Data;
 
 [assembly: HostingStartup(typeof(MyApp.ConfigureValidation))]
 
-namespace MyApp
+namespace MyApp;
+
+public class ConfigureValidation : IHostingStartup
 {
-    public class ConfigureValidation : IHostingStartup
-    {
-        // Add support for dynamically generated db rules
-        public void Configure(IWebHostBuilder builder) => builder
-            .ConfigureServices(services => services.AddSingleton<IValidationSource>(c =>
-                new OrmLiteValidationSource(c.Resolve<IDbConnectionFactory>())))
-            .ConfigureAppHost(appHost => {
-                appHost.Resolve<IValidationSource>().InitSchema();
-            });
-    }
+    // Add support for dynamically generated db rules
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices(services => services.AddSingleton<IValidationSource>(c =>
+            new OrmLiteValidationSource(c.Resolve<IDbConnectionFactory>())))
+        .ConfigureAppHost(appHost => {
+            appHost.Resolve<IValidationSource>().InitSchema();
+        });
 }
