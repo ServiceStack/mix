1bb9f74501228a0201203b5a6e53e0b8bc166b5d
Demis Bellot <demis.bellot@gmail.com>
2022-07-16 02:38:46 +0800

    Add profiling

---
+++ features/profiling/Configure.Profiling.cs
@@ -0,0 +1,22 @@
+using ServiceStack;
+
+[assembly: HostingStartup(typeof(MyApp.ConfigureProfiling))]
+
+namespace MyApp;
+
+public class ConfigureProfiling : IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppHost(
+            host => {
+                host.Plugins.AddIfDebug(new RequestLogsFeature {
+                    EnableResponseTracking = true,
+                });
+                
+                host.Plugins.AddIfDebug(new ProfilingFeature {
+                    IncludeStackTrace = true,
+                });
+            });
+    }
+}
