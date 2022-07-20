298649ff96dfd40ae718c0e467132d8dd1728a64
Demis Bellot <demis.bellot@gmail.com>
2022-07-20 15:35:29 +0800

    Update Configure.Profiling.cs

--- features/profiling/Configure.Profiling.cs
+++ features/profiling/Configure.Profiling.cs
@@ -8,15 +8,14 @@ public class ConfigureProfiling : IHostingStartup
 {
     public void Configure(IWebHostBuilder builder)
     {
-        builder.ConfigureAppHost(
-            host => {
-                host.Plugins.AddIfDebug(new RequestLogsFeature {
-                    EnableResponseTracking = true,
-                });
-                
-                host.Plugins.AddIfDebug(new ProfilingFeature {
-                    IncludeStackTrace = true,
-                });
+        builder.ConfigureAppHost(host => {
+            host.Plugins.AddIfDebug(new RequestLogsFeature {
+                EnableResponseTracking = true,
             });
+            
+            host.Plugins.AddIfDebug(new ProfilingFeature {
+                IncludeStackTrace = true,
+            });
+        });
     }
 }
