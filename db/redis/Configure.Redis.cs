4d19857036921c81740351e8765bc75849173fed
Demis Bellot <demis.bellot@gmail.com>
2022-08-18 16:04:14 +0800

    Update Configure.Redis.cs

--- db/redis/Configure.Redis.cs
+++ db/redis/Configure.Redis.cs
@@ -3,14 +3,17 @@ using ServiceStack.Redis;
 
 [assembly: HostingStartup(typeof(MyApp.ConfigureRedis))]
 
-namespace MyApp
+namespace MyApp;
+
+public class ConfigureRedis : IHostingStartup
 {
-    public class ConfigureRedis : IHostingStartup
-    {
-        public void Configure(IWebHostBuilder builder) => builder
-            .ConfigureServices((context, services) => {
-                services.AddSingleton<IRedisClientsManager>(
-                    new RedisManagerPool(context.Configuration.GetConnectionString("Redis") ?? "localhost:6379"));
-            });
-    }
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices((context, services) => {
+            services.AddSingleton<IRedisClientsManager>(
+                new RedisManagerPool(context.Configuration.GetConnectionString("Redis") ?? "localhost:6379"));
+        })
+        .ConfigureAppHost(appHost => {
+            // Enable built-in Redis Admin UI at /admin-ui/redis
+            // appHost.Plugins.Add(new AdminRedisFeature());
+        });
 }
