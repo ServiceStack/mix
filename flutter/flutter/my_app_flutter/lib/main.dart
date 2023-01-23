53740290068e8c347a9f618bca0711e86a85078a
Darren Reid <darren@reidmail.org>
2023-01-23 17:00:25 +1100

    Cleanup flutter mix

--- flutter/flutter/my_app_flutter/lib/main.dart
+++ flutter/flutter/my_app_flutter/lib/main.dart
@@ -1,25 +1,23 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
-import 'package:servicestack/client.dart';
+import 'package:flutter/material.dart';
+import 'package:servicestack/web_client.dart' if (dart.library.io) 'package:servicestack/client.dart';
 import 'dart:io';
 
 import 'dtos.dart';
 
 var baseUrl = "https://localhost:5001";
-
-var client = JsonServiceClient(baseUrl);
+var clientOptions = ClientOptions(baseUrl: baseUrl, ignoreCert: true);
+var client = ClientFactory.createWith(clientOptions);
 
 void main() {
-  if (!kReleaseMode) {
-    HttpOverrides.global = MyHttpOverrides();
-    if(Platform.isAndroid) {
-      baseUrl = "http://10.0.2.2:5000";
-    } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-      baseUrl = "https://localhost:5001";
-    } else if (Platform.isIOS) {
-      baseUrl = "https://localhost:5001";
+  if (!kReleaseMode && !kIsWeb) {
+    if (Platform.isAndroid) {
+      clientOptions.baseUrl = "https://10.0.2.2:5001";
+      clientOptions.ignoreCertificatesFor.add(clientOptions.baseUrl);
     }
   }
+
+  client = ClientFactory.createWith(clientOptions);
   runApp(const MyApp());
 }
 
@@ -91,7 +89,8 @@ class HelloFlutterState extends State<HelloFlutter> {
   }
 
   void callService() {
-    client.get(Hello(name: myController.text)).then((value) => setState(() {
+    var text = myController.text.isEmpty ? "World" : myController.text;
+    client.get(Hello(name: text)).then((value) => setState(() {
       result = value.result!;
     }));
   }
@@ -106,30 +105,43 @@ class HelloFlutterState extends State<HelloFlutter> {
         ),
       body: Center(
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              const Text('Hello API', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24.0),),
-              Container(
-                  margin: const EdgeInsets.only(left:100.0,right:100.0),
-                  child: TextField(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 20),
+              margin: const EdgeInsets.only(left: 100.0, right: 100.0),
+              child: Column(
+                children: [
+                  const Text(
+                    'Hello API',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
+                  ),
+                  TextField(
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'World',
                     ),
-                  controller: myController,
-                ),
+                    controller: myController,
+                  )
+                ],
               ),
-              Text('Result: $result',
-              style: const TextStyle(fontStyle: FontStyle.italic),),
-            ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Result: $result',
+                style: const TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -138,15 +150,5 @@ class HelloFlutterState extends State<HelloFlutter> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-}
-
-/// Use only in debug to make it easy to access localhost API service.
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host,
-          int port) => true;
   }
 }
