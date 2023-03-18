55cc6987af3a03d67caf8ef99e87da915c7fee0b
Demis Bellot <demis.bellot@gmail.com>
2023-03-18 12:40:17 +0800

    Update autoquery mix template

--- features/autoquery/Configure.AutoQuery.cs
+++ features/autoquery/Configure.AutoQuery.cs
@@ -2,16 +2,15 @@ using ServiceStack;
 
 [assembly: HostingStartup(typeof(MyApp.ConfigureAutoQuery))]
 
-namespace MyApp
+namespace MyApp;
+
+public class ConfigureAutoQuery : IHostingStartup
 {
-    public class ConfigureAutoQuery : IHostingStartup
-    {
-        public void Configure(IWebHostBuilder builder) => builder
-            .ConfigureAppHost(appHost => {
-                appHost.Plugins.Add(new AutoQueryFeature {
-                    MaxLimit = 1000,
-                    //IncludeTotal = true,
-                });
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureAppHost(appHost => {
+            appHost.Plugins.Add(new AutoQueryFeature {
+                MaxLimit = 1000,
+                //IncludeTotal = true,
             });
-    }
+        });
 }
