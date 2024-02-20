4558fc14f2c00e379bf8c094fe79b82b73ff80b2
Demis Bellot <demis.bellot@gmail.com>
2024-02-20 17:41:20 +0800

    Update Configure.Cors.cs

--- features/cors/Configure.Cors.cs
+++ features/cors/Configure.Cors.cs
@@ -1,5 +1,3 @@
-using ServiceStack;
-
 [assembly: HostingStartup(typeof(MyApp.ConfigureCors))]
 
 namespace MyApp;
@@ -7,7 +5,28 @@ namespace MyApp;
 public class ConfigureCors : IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
-        .ConfigureServices(services => {
-            services.AddPlugin(new CorsFeature());
+        .ConfigureServices(services =>
+        {
+            services.AddCors(options => {
+                options.AddDefaultPolicy(policy => {
+                    policy.WithOrigins([
+                        "http://localhost:5000", "https://localhost:5001", 
+                        "https://localhost:5173", "http://localhost:5173",
+                    ])
+                    .AllowCredentials()
+                    .WithHeaders(["Content-Type", "Allow", "Authorization"])
+                    .SetPreflightMaxAge(TimeSpan.FromHours(1));
+                });
+            });
+            services.AddTransient<IStartupFilter, StartupFilter>();
         });
+
+    public class StartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next) => app =>
+        {
+            app.UseCors();
+            next(app);
+        };
+    }        
 }
