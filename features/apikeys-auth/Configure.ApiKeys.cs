31b7df71cd266db48bb021bd593b10116ab81605
Demis Bellot <demis.bellot@gmail.com>
2024-06-16 11:34:07 +0800

    Update Configure.ApiKeys.cs

--- features/apikeys-auth/Configure.ApiKeys.cs
+++ features/apikeys-auth/Configure.ApiKeys.cs
@@ -1,9 +1,7 @@
-using MyApp.Data;
 using ServiceStack;
 using ServiceStack.Auth;
 using ServiceStack.Data;
 using ServiceStack.OrmLite;
-using ServiceStack.Configuration;
 
 [assembly: HostingStartup(typeof(MyApp.ConfigureApiKeys))]
 
@@ -14,7 +12,7 @@ public class ConfigureApiKeys : IHostingStartup
     public void Configure(IWebHostBuilder builder) => builder
         .ConfigureServices(services =>
         {
-            services.AddPlugin(new AuthFeature(new AuthSecretAuthProvider()));
+            services.AddPlugin(new AuthFeature(new AuthSecretAuthProvider("p@55wOrd")));
             services.AddPlugin(new ApiKeysFeature
             {
                 // Optional: Available Scopes Admin Users can assign to any API Key
@@ -27,21 +25,10 @@ public class ConfigureApiKeys : IHostingStartup
                 //     "todo:read",
                 //     "todo:write",
                 // ],
-                
-                // Optional: Limit available Scopes Users can assign to their own API Keys
-                // UserScopes = [
-                //     "todo:read",
-                // ],
-                // Optional: Limit available Features Users can assign to their own API Keys
-                // UserFeatures = [
-                //     "Tracking",
-                // ],
             });
         })
         .ConfigureAppHost(appHost =>
         {
-            appHost.Config.AdminAuthSecret = "p@55wOrd";
-            
             using var db = appHost.Resolve<IDbConnectionFactory>().Open();
             var feature = appHost.GetPlugin<ApiKeysFeature>();
             feature.InitSchema(db);
