249a5eccb6c820d45a284d8d6f5a33f809fe686b
Demis Bellot <demis.bellot@gmail.com>
2024-09-03 10:28:41 +0800

    Add jobs

---
+++ features/jobs/Configure.BackgroundJobs.cs
@@ -0,0 +1,35 @@
+using ServiceStack.Data;
+using ServiceStack.Jobs;
+using ServiceStack.Web;
+
+[assembly: HostingStartup(typeof(MyApp.ConfigureBackgroundJobs))]
+
+namespace MyApp;
+
+public class ConfigureBackgroundJobs : IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices(services => {
+            services.AddPlugin(new CommandsFeature());
+            services.AddPlugin(new BackgroundsJobFeature());
+            services.AddHostedService<JobsHostedService>();
+         }).ConfigureAppHost(afterAppHostInit: appHost => {
+            var jobs = services.GetRequiredService<IBackgroundJobs>();
+            // Example of registering a Recurring Job to run Every Hour
+            //jobs.RecurringCommand<MyCommand>(Schedule.Hourly);
+        });
+}
+
+public class JobsHostedService(ILogger<JobsHostedService> log, IBackgroundJobs jobs) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await jobs.StartAsync(stoppingToken);
+        
+        using var timer = new PeriodicTimer(TimeSpan.FromSeconds(3));
+        while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            await jobs.TickAsync();
+        }
+    }
+}
